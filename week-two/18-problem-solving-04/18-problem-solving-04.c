63f9346e024e0fd38464f24d26ace827a9e4ef59
Polashahmad01 <dev.polashahmad@gmail.com>
2024-03-03 15:46:54 +0600

    Add code for multiplication tables and unique numbers

--- week-two/18-problem-solving-04/18-problem-solving-04.c
+++ week-two/18-problem-solving-04/18-problem-solving-04.c
@@ -2,5 +2,19 @@
 
 int main()
 {
+    int n, m, i, j;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &m);
+        for(j = 10; j <= m; j++) {
+            if(j % 11 == 0) {
+                continue;
+            }
+            printf("%d\n", j);
+        }
+    }
+
     return 0;
 }
