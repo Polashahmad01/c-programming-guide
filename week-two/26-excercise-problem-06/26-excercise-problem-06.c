6dec848b82fccada495a58d275eac2e1ec78682a
Polashahmad01 <dev.polashahmad@gmail.com>
2024-03-04 21:13:00 +0600

    Add solutions for problems 05 and 06

---
+++ week-two/26-excercise-problem-06/26-excercise-problem-06.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, m, i;
+
+    scanf("%d%d", &n, &m);
+
+    if(n > 0 && m > 0) {
+        if(n < m) {
+            for(i = n; i <= m; i++) {
+                printf("%d ", i);
+            }
+        }
+        else {
+            for(i = n; i >= m; i--) {
+                printf("%d ", i);
+            }
+        }
+    }
+
+    return 0;
+}
